81691b385a27539055462f9ba2d48723d1cdfafd
Przemyslaw Sagalo <przemyslawsagalo@MacBook-Pro-Przemyslaw.local>
2022-12-14 22:46:57 +0100

    new configuratio from different source

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
-require("config")
+require "config.options"
+require "config.keymaps"
+require "config.packer"
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -3,4 +3,11 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
 end)
