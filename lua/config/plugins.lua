222bc2d412c3077cac7760e76d7908e27163b70a
Przemyslaw Sagalo <przemyslaw.sagalo@gmail.com>
2022-12-21 22:30:33 +0000

    nvimtree; autopairs

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -45,6 +45,17 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use 'folke/tokyonight.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
