95bfdd85e8eec14007ac9f27eef208cde84728ee
Przemyslaw <przemyslaw.sagalo@mail.com>
2023-01-13 17:46:13 +0000

    lsp default servers

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,18 @@
+-- Reserve space for diagnostic icons
+vim.opt.signcolumn = 'yes'
+
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'pyright'
+})
+
+-- Configure lua language server for neovim
+lsp.nvim_workspace()
 
 lsp.setup()
