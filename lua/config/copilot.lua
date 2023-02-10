1a538236b3c12748b83b7d6a445a36b3d8f43c8f
Przemyslaw Sagalo <przemyslaw.sagalo@gmail.com>
2023-02-10 21:58:31 +0100

    fixed bug dap and gihtubxopilot

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -1,4 +1,4 @@
-vim.g.copilot_filetypes = { ["*"] = true }
+vim.g.copilot_filetypes = { ["*"] = false, lua = true, python = true, html = true, markdown = true, json = true, yaml = true, sh = true, zsh = true, bash = true, vim = true, go = true, rust = true, c = true, cpp = true, kotlin = true, sql = true, yaml = true}
 
 vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
 
