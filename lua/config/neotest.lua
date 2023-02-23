3f4742d18774f2c83f4239278c06eadcf791f4e0
Przemyslaw Sagalo <przemyslaw.sagalo@gmail.com>
2023-02-23 22:36:26 +0100

    neotest keymaps

--- lua/config/neotest.lua
+++ lua/config/neotest.lua
@@ -9,3 +9,11 @@ require("neotest").setup({
     }),
   },
 })
+
+vim.keymap.set("n", "<leader>tm", ':lua require("neotest").run.run()<CR>')
+vim.keymap.set("n", "<leader>tf", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>')
+vim.keymap.set("n", "<leader>ta", ':lua require("neotest").run.run(vim.fn.expand("."))<CR>')
+vim.keymap.set("n", "<leader>td", ':lua require("neotest").run.run({strategy = "dap"})<CR>')
+vim.keymap.set("n", "<leader>ts", ':lua require("neotest").run.stop()<CR>')
+vim.keymap.set("n", "<leader>tt", ':lua require("neotest").summary.toggle()<CR> :lua require("neotest").output_panel.toggle()<CR>')
+
