6b91827134d21e50ff3280e9b758c4ecf52803d8
Przemyslaw Sagalo <przemek@sagalo.pro>
2025-11-19 22:31:00 +0100

    feat: Added aerial extension

---
+++ lua/plugins/configs/aerial.lua
@@ -0,0 +1,14 @@
+local status_ok, aerial = pcall(require, "aerial")
+
+if not status_ok then
+  return
+end
+
+aerial.setup({
+  on_attach = function(bufnr)
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
