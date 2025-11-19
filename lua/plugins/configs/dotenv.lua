e1aaa8c0cc17be98fffa506cc760ea950eac454b
Przemyslaw Sagalo <przemek@sagalo.pro>
2025-11-19 20:53:27 +0000

    refactor: New config for dotenv plugin

--- lua/plugins/configs/dotenv.lua
+++ lua/plugins/configs/dotenv.lua
@@ -2,3 +2,14 @@ require('dotenv').setup({
   enable_on_load = true, -- will load your .env file upon loading a buffer
   verbose = true, -- show error notification if .env file is not found and if .env is loaded
 })
+
+local status, dotenv = pcall(require, "dotenv")
+
+if not status then
+  vim.notify("Dotenv plugin not found. Skipping.", vim.log.levels.WARN)
+else
+  dotenv.setup({
+    enable_on_load = true,
+    verbose = false
+  })
+end
