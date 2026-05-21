ae9bec795b44f93cffc63b8e04b508e6ffac2925
PrzemyslawSagalo <przemek@sagalo.pro>
2026-05-21 19:06:00 +0000

    feat(config): implement hyper-fast non-blocking file autoread

--- lua/plugins/configs/vim_options.lua
+++ lua/plugins/configs/vim_options.lua
@@ -15,3 +15,19 @@ vim.opt.incsearch = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+-- Hyper-fast autoread configuration
+vim.opt.autoread = true
+vim.opt.updatetime = 200
+
+local autoread_group = vim.api.nvim_create_augroup("HyperFastAutoread", { clear = true })
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorMoved" }, {
+	group = autoread_group,
+	desc = "Synchronize file state with disk on focus and movement",
+	callback = function()
+		if vim.api.nvim_get_mode().mode ~= "c" then
+			vim.cmd("checktime")
+		end
+	end,
+})
